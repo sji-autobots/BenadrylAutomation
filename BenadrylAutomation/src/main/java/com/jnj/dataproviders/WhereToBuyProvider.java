6270d900c6e17ae643bf1d73aaecdcb08e5ff8d5
goutamnaik007 <goutam.naik@sjinnovation.com>
2024-04-03 17:27:23 +0530

    Added all tests for Where To Buy module

--- BenadrylAutomation/src/main/java/com/jnj/dataproviders/WhereToBuyProvider.java
+++ BenadrylAutomation/src/main/java/com/jnj/dataproviders/WhereToBuyProvider.java
@@ -1,3 +1,8 @@
+/**
+ * @author Goutam Naik
+ * @date 27-March-24
+ */
+
 package com.jnj.dataproviders;
 
 import java.io.IOException;
@@ -7,6 +12,7 @@ import org.apache.poi.ss.usermodel.DataFormatter;
 import com.jnj.utility.DataProvider;
 
 public class WhereToBuyProvider {
+	
 	DataFormatter formatter = new DataFormatter();
 	String dataPath = "src\\test\\resources\\TestData\\whereToBuyTestData.xlsx";
 
@@ -15,5 +21,25 @@ public class WhereToBuyProvider {
 	@org.testng.annotations.DataProvider(name = "headerVerification")
 	public Object[][] headerVerification() throws IOException {
 		return provider.getData(dataPath, "headerVerification");
+	}
+	
+	@org.testng.annotations.DataProvider(name = "productSearch")
+	public Object[][] productSearch() throws IOException {
+		return provider.getData(dataPath, "productSearch");
+	}
+	
+	@org.testng.annotations.DataProvider(name = "changeCategorySize")
+	public Object[][] changeCategorySize() throws IOException {
+		return provider.getData(dataPath, "changeCategorySize");
+	}
+	
+	@org.testng.annotations.DataProvider(name = "verifyStores")
+	public Object[][] verifyStores() throws IOException {
+		return provider.getData(dataPath, "verifyStores");
+	}
+	
+	@org.testng.annotations.DataProvider(name = "verifyLocations")
+	public Object[][] verifyLocations() throws IOException {
+		return provider.getData(dataPath, "verifyLocations");
 	}
 }
--- BenadrylAutomation/src/test/java/com/jnj/testcases/WhereToBuyTest.java
+++ BenadrylAutomation/src/test/java/com/jnj/testcases/WhereToBuyTest.java
@@ -1,3 +1,8 @@
+/**
+ * @author Goutam Naik
+ * @date 27-March-24
+ */
+
 package com.jnj.testcases;
 
 import org.testng.SkipException;
@@ -8,28 +13,88 @@ import org.testng.annotations.Test;
 import com.jnj.base.BaseClass;
 import com.jnj.dataproviders.WhereToBuyProvider;
 
-public class WhereToBuyTest extends BaseClass{
-	
+public class WhereToBuyTest extends BaseClass {
+
 	@BeforeMethod
 	public void setup() throws InterruptedException {
 		launchApplication();
 	}
 
 	@Test(priority = 1, dataProvider = "headerVerification", dataProviderClass = WhereToBuyProvider.class)
-	public void HEADER_verifyMainMenuLink(String testcase, String execution, String expectedUrl, String expectedHeader) {
+	public void WTB_verifyHeader(String testcase, String execution, String expectedUrl, String expectedHeader) {
 		test = test.createNode(testcase);
 		if (execution.equalsIgnoreCase(defaultFlag)) {
 			selectEnv(runOn);
 			home.closePrivacyPopup();
 			header.verifyWhereToBuyLink(expectedUrl);
+			wtb.verifyHeading(expectedHeader);
 		} else {
 			throw new SkipException("Test skipped : " + testcase);
 		}
 	}
-	
+
+	@Test(priority = 2, dataProvider = "productSearch", dataProviderClass = WhereToBuyProvider.class)
+	public void WTB_verifyProductSearch(String testcase, String execution, String expectedUrl, String productKey) {
+		test = test.createNode(testcase);
+		if (execution.equalsIgnoreCase(defaultFlag)) {
+			selectEnv(runOn);
+			home.closePrivacyPopup();
+			header.verifyWhereToBuyLink(expectedUrl);
+			wtb.searchProduct(productKey);
+			wtb.verifyCategoryAndSize();
+		} else {
+			throw new SkipException("Test skipped : " + testcase);
+		}
+	}
+
+	@Test(priority = 3, dataProvider = "changeCategorySize", dataProviderClass = WhereToBuyProvider.class)
+	public void WTB_verifyCategoryAndSizeChanges(String testcase, String execution, String expectedUrl,
+			String productKey, String category, String size, String expectedNewProduct) {
+		test = test.createNode(testcase);
+		if (execution.equalsIgnoreCase(defaultFlag)) {
+			selectEnv(runOn);
+			home.closePrivacyPopup();
+			header.verifyWhereToBuyLink(expectedUrl);
+			wtb.searchProduct(productKey);
+			wtb.changeProductOptions(category, size);
+			wtb.verifyProductName(expectedNewProduct);
+		} else {
+			throw new SkipException("Test skipped : " + testcase);
+		}
+	}
+
+	@Test(priority = 4, dataProvider = "verifyStores", dataProviderClass = WhereToBuyProvider.class)
+	public void WTB_verifyStoresRedirection(String testcase, String execution, String expectedUrl, String location,
+			String storeName, String expectedStoreUrl) {
+		test = test.createNode(testcase);
+		if (execution.equalsIgnoreCase(defaultFlag)) {
+			selectEnv(runOn);
+			home.closePrivacyPopup();
+			header.verifyWhereToBuyLink(expectedUrl);
+			wtb.searchLocation(location);
+			wtb.checkStoreRedirection(storeName, expectedStoreUrl);
+		} else {
+			throw new SkipException("Test skipped : " + testcase);
+		}
+	}
+
+	@Test(priority = 5, dataProvider = "verifyLocations", dataProviderClass = WhereToBuyProvider.class)
+	public void WTB_verifyRelevantStoreLocationsDisplayed(String testcase, String execution, String expectedUrl,
+			String location) {
+		test = test.createNode(testcase);
+		if (execution.equalsIgnoreCase(defaultFlag)) {
+			selectEnv(runOn);
+			home.closePrivacyPopup();
+			header.verifyWhereToBuyLink(expectedUrl);
+			wtb.searchLocation(location);
+			wtb.verifyAddressesForStores(location);
+		} else {
+			throw new SkipException("Test skipped : " + testcase);
+		}
+	}
+
 	@AfterMethod
 	public void tearDown() {
 		driver.quit();
 	}
-
 }
