672175230060b5e0eacaf33d502a981b5ce4dd66
zahinSJI <zahin.ahad@sjinnovation.com>
2024-04-22 15:44:09 +0600

    Locators made private, code formatted, redundant imports removed

--- BenadrylAutomation/src/main/java/com/jnj/pageobjects/ListingPage.java
+++ BenadrylAutomation/src/main/java/com/jnj/pageobjects/ListingPage.java
@@ -15,135 +15,137 @@ import org.testng.Assert;
 import com.jnj.actions.Action;
 import com.jnj.base.BaseClass;
 
-public class ListingPage extends BaseClass{
-    Actions actions;
+public class ListingPage extends BaseClass {
+	Actions actions;
 
-    /**
-     * Constructor
-     */
-    public ListingPage() {
-        PageFactory.initElements(driver, this);
-        actions = new Actions(driver);
-    }
+	/**
+	 * Constructor
+	 */
+	public ListingPage() {
+		PageFactory.initElements(driver, this);
+		actions = new Actions(driver);
+	}
 
-    /**
-     * Locators 
-     */
-    @FindBy(css = "#tab-menu-8651")
-    WebElement prodHeaderMenu;
-    
-    @FindBy(css = "#file-926 img")
-    WebElement bannerImage;
-    
-    @FindBy(css = ".inner > #content-main")
-    WebElement bannerHeading;
+	/**
+	 * Locators
+	 */
+	@FindBy(css = "#tab-menu-8651")
+	private WebElement prodHeaderMenu;
 
-    @FindBy(css = ".inner p")
-    WebElement bannerSubHeading;
+	@FindBy(css = "#file-926 img")
+	private WebElement bannerImage;
 
-    @FindBy(css = "[data-value='56']")
-    WebElement adultOralQuickFilter;
+	@FindBy(css = ".inner > #content-main")
+	private WebElement bannerHeading;
 
-    @FindBy(css = "[data-value='711']")
-    WebElement adultTopicalQuickFilter;
+	@FindBy(css = ".inner p")
+	private WebElement bannerSubHeading;
 
-    @FindBy(css = "[data-value='61']")
-    WebElement kidsProdQuickFilter;
-	
+	@FindBy(css = "[data-value='56']")
+	private WebElement adultOralQuickFilter;
+
+	@FindBy(css = "[data-value='711']")
+	private WebElement adultTopicalQuickFilter;
+
+	@FindBy(css = "[data-value='61']")
+	private WebElement kidsProdQuickFilter;
+
 	@FindBy(xpath = "//div[@id='onetrust-close-btn-container']")
-	WebElement closePrivacyBtn;
-	
+	private WebElement closePrivacyBtn;
+
 	@FindBy(css = ".main-row .three-quarters")
-	WebElement productsPanel;
-	
+	private WebElement productsPanel;
+
 	@FindBy(id = "edit-sort-by")
-	WebElement sortBySelector;
-	
+	private WebElement sortBySelector;
+
 	@FindBy(css = ".sfmc-careclub-lightbox-close")
 	private WebElement closeSignUpBtn;
-    
-    public WebElement getBanner(String text) {
-        return driver.findElement(By.xpath("//div[@class='inner']/*[contains(.,'" + text + "')]"));
-    }
-    
-    public WebElement getFilter(String text) {
-        return driver.findElement(By.xpath("//div[@class='filter-row-shortcuts']/a[contains(.,'" + text + "')]"));
-    }
-	
+
+	private WebElement getBanner(String text) {
+		return driver.findElement(By.xpath("//div[@class='inner']/*[contains(.,'" + text + "')]"));
+	}
+
+	private WebElement getFilter(String text) {
+		return driver.findElement(By.xpath("//div[@class='filter-row-shortcuts']/a[contains(.,'" + text + "')]"));
+	}
+
 	/**
 	 * Function to click on close privacy pop-up button
 	 */
 	public void closePrivacyPopup() {
 		Action.explicitWaitForElementTobeclickable(closePrivacyBtn, 30);
 		boolean eleDisplayed = closePrivacyBtn.isDisplayed();
-		if(eleDisplayed) {
+		if (eleDisplayed) {
 			extentPassLog("Privacy pop-up displayed : ", true);
 			Action.performActionwithExtentInfoLog(closePrivacyBtn, "click", "Clicking on : Close button");
-		} else extentFailLog("Privacy pop-up displayed : ", false);
+		} else
+			extentFailLog("Privacy pop-up displayed : ", false);
 	}
-	
+
 	/**
 	 * Function to click on close sign up pop-up button
 	 */
 	public void closeSignupPopup() {
 		Action.explicitWaitForElementTobeclickable(closeSignUpBtn, 30);
 		boolean eleDisplayed = closeSignUpBtn.isDisplayed();
-		if(eleDisplayed) {
+		if (eleDisplayed) {
 			extentPassLog("Privacy pop-up displayed : ", true);
 			Action.performActionwithExtentInfoLog(closeSignUpBtn, "click", "Clicking on : Close button");
-		} else extentFailLog("Signup pop-up displayed : ", false);
+		} else
+			extentFailLog("Signup pop-up displayed : ", false);
 	}
 
-    /**
-     * Visits a product listing page from the homepage.
-     */
-    public void visitPLP() {
-        Action.performActionwithExtentInfoLog(prodHeaderMenu, "click", "Clicking on : " + prodHeaderMenu.getText());
-    }
+	/**
+	 * Visits a product listing page from the homepage.
+	 */
+	public void visitPLP() {
+		Action.performActionwithExtentInfoLog(prodHeaderMenu, "click", "Clicking on : " + prodHeaderMenu.getText());
+	}
 
-    /**
-     * Visits a listing page from the homepage.
-     * Then, verifies the banner image in the listing page.
-     */
-    public void verifyBannerImage() {
-    	this.visitPLP();
-        extentInfoLog("Image is displayed : ", Action.isDisplayed(driver, bannerImage));
-    }
+	/**
+	 * Visits a listing page from the homepage. Then, verifies the banner image in
+	 * the listing page.
+	 */
+	public void verifyBannerImage() {
+		this.visitPLP();
+		extentInfoLog("Image is displayed : ", Action.isDisplayed(driver, bannerImage));
+	}
 
-    /**
-     * Visits a listing page from the homepage.
-     * Then, verifies the banner texts in the listing page.
-     */
-    public void verifyBannerTexts(String text) {
-    	this.visitPLP();
-		WebElement actualElem= getBanner(text);
-		String actualText= actualElem.getText();
-        extentInfoLog("Banner heading is displayed : ", Action.isDisplayed(driver, actualElem));
-        Assert.assertEquals(actualText, text);
-        extentInfoLog("Banner text verified");
-    }
+	/**
+	 * Visits a listing page from the homepage. Then, verifies the banner texts in
+	 * the listing page.
+	 */
+	public void verifyBannerTexts(String text) {
+		this.visitPLP();
+		WebElement actualElem = getBanner(text);
+		String actualText = actualElem.getText();
+		extentInfoLog("Banner heading is displayed : ", Action.isDisplayed(driver, actualElem));
+		Assert.assertEquals(actualText, text);
+		extentInfoLog("Banner text verified");
+	}
 
-    /**
-     * Visits a listing page from the homepage.
-     * Then, verifies the filters in the listing page.
-     */
-    public void verifyQuickFilters(String text) {
-    	this.visitPLP();
-		WebElement actualElem= getFilter(text);
-		String actualText= actualElem.getText();
-        extentInfoLog("Filter text is displayed : ", Action.isDisplayed(driver, actualElem));
+	/**
+	 * Visits a listing page from the homepage. Then, verifies the filters in the
+	 * listing page.
+	 */
+	public void verifyQuickFilters(String text) {
+		this.visitPLP();
+		WebElement actualElem = getFilter(text);
+		String actualText = actualElem.getText();
+		extentInfoLog("Filter text is displayed : ", Action.isDisplayed(driver, actualElem));
 		Action.performActionwithExtentInfoLog(actualElem, "click", "Clicking on : " + actualText);
-        extentInfoLog("Products are displayed : ", Action.isDisplayed(driver, productsPanel));
-    }
+		extentInfoLog("Products are displayed : ", Action.isDisplayed(driver, productsPanel));
+	}
 
-    /**
-     * Visits a listing page from the homepage.
-     * Then, verifies the sort by selector the listing page.
-     */
-    public void verifySortBy(String text) {
-    	this.visitPLP();
-      extentInfoLog("Sort by displayed : ", Action.isDisplayed(driver, sortBySelector));
-      Action.selectByValue(sortBySelector, text);
-        extentInfoLog("Products are displayed : ", Action.isDisplayed(driver, productsPanel));
-    }
+	/**
+	 * Visits a listing page from the homepage. Then, verifies the sort by selector
+	 * the listing page.
+	 */
+	public void verifySortBy(String text) {
+		this.visitPLP();
+		extentInfoLog("Sort by displayed : ", Action.isDisplayed(driver, sortBySelector));
+		Action.selectByValue(sortBySelector, text);
+		extentInfoLog("Products are displayed : ", Action.isDisplayed(driver, productsPanel));
+	}
 }
--- BenadrylAutomation/src/test/java/com/jnj/testcases/ListingPageTest.java
+++ BenadrylAutomation/src/test/java/com/jnj/testcases/ListingPageTest.java
@@ -7,68 +7,65 @@ package com.jnj.testcases;
 
 import org.testng.SkipException;
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import com.jnj.base.BaseClass;
 import com.jnj.dataproviders.ListingPageProvider;
-import com.jnj.dataproviders.ProductPageProvider;
 
 public class ListingPageTest extends BaseClass {
-    @BeforeClass
-    public void setup() throws InterruptedException {
-        launchApplication();
-    }
+	@BeforeClass
+	public void setup() throws InterruptedException {
+		launchApplication();
+	}
 
-    @Test(priority = 1, dataProvider = "bannerImg", dataProviderClass = ListingPageProvider.class)
-    public void PLP_verifyBannerImage(String testcase, String execution) {
-        test = test.createNode(testcase);
-        if (execution.equalsIgnoreCase(defaultFlag)) {
+	@Test(priority = 1, dataProvider = "bannerImg", dataProviderClass = ListingPageProvider.class)
+	public void PLP_verifyBannerImage(String testcase, String execution) {
+		test = test.createNode(testcase);
+		if (execution.equalsIgnoreCase(defaultFlag)) {
 			selectEnv(runOn);
 			home.closePrivacyPopup();
-            plp.verifyBannerImage();
-        } else {
-            throw new SkipException("Test skipped : " + testcase);
-        }
-    }
+			plp.verifyBannerImage();
+		} else {
+			throw new SkipException("Test skipped : " + testcase);
+		}
+	}
 
-    @Test(priority = 2, dataProvider = "bannerTexts", dataProviderClass = ListingPageProvider.class)
-    public void PLP_verifyBannerTexts(String testcase, String execution, String expectedText) {
-        test = test.createNode(testcase);
-        if (execution.equalsIgnoreCase(defaultFlag)) {
+	@Test(priority = 2, dataProvider = "bannerTexts", dataProviderClass = ListingPageProvider.class)
+	public void PLP_verifyBannerTexts(String testcase, String execution, String expectedText) {
+		test = test.createNode(testcase);
+		if (execution.equalsIgnoreCase(defaultFlag)) {
 			selectEnv(runOn);
-            plp.verifyBannerTexts(expectedText);
-        } else {
-            throw new SkipException("Test skipped : " + testcase);
-        }
-    }
+			plp.verifyBannerTexts(expectedText);
+		} else {
+			throw new SkipException("Test skipped : " + testcase);
+		}
+	}
 
-    @Test(priority = 3, dataProvider = "quickFilter", dataProviderClass = ListingPageProvider.class)
-    public void PLP_verifyQuickFilters(String testcase, String execution, String expectedText) {
-        test = test.createNode(testcase);
-        if (execution.equalsIgnoreCase(defaultFlag)) {
+	@Test(priority = 3, dataProvider = "quickFilter", dataProviderClass = ListingPageProvider.class)
+	public void PLP_verifyQuickFilters(String testcase, String execution, String expectedText) {
+		test = test.createNode(testcase);
+		if (execution.equalsIgnoreCase(defaultFlag)) {
 			selectEnv(runOn);
-            plp.verifyQuickFilters(expectedText);
-        } else {
-            throw new SkipException("Test skipped : " + testcase);
-        }
-    }
+			plp.verifyQuickFilters(expectedText);
+		} else {
+			throw new SkipException("Test skipped : " + testcase);
+		}
+	}
 
-    @Test(priority = 4, dataProvider = "sortBy", dataProviderClass = ListingPageProvider.class)
-    public void PLP_verifySortBy(String testcase, String execution, String selectorText) {
-        test = test.createNode(testcase);
-        if (execution.equalsIgnoreCase(defaultFlag)) {
+	@Test(priority = 4, dataProvider = "sortBy", dataProviderClass = ListingPageProvider.class)
+	public void PLP_verifySortBy(String testcase, String execution, String selectorText) {
+		test = test.createNode(testcase);
+		if (execution.equalsIgnoreCase(defaultFlag)) {
 			selectEnv(runOn);
-            plp.verifySortBy(selectorText);
-        } else {
-            throw new SkipException("Test skipped : " + testcase);
-        }
-    }
+			plp.verifySortBy(selectorText);
+		} else {
+			throw new SkipException("Test skipped : " + testcase);
+		}
+	}
 
-    @AfterClass
-    public void tearDown() {
-        driver.quit();
-    }
+	@AfterClass
+	public void tearDown() {
+		driver.quit();
+	}
 }
